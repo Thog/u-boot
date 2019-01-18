OE4T/u-boot-tegra

T210: Add Kconfig switch CPU_BL_IS_CBOOT
This allows two T210 builds to be created:

One that still expects to be loaded by NVTboot_CPU (legacy).
One that expects to be loaded like a kernel via CBoot al la t186.

Change-Id: I08a04d1518efc793154e464c2e04bb8bfd6c0501
Signed-off-by: Tom Warren <twarren@nvidia.com>
Reviewed-on: http://git-master/r/1272779
GVS: Gerrit_Virtual_Submit
Reviewed-by: Jimmy Zhang <jimmzhang@nvidia.com>

include/configs/tegra210-common.h
@@ -26,7 +26,11 @@
 /*-----------------------------------------------------------------------
  * Physical Memory Map
  */
+#ifdef CONFIG_CPU_BL_IS_CBOOT
+#define CONFIG_SYS_TEXT_BASE	0x80080000	/* chainloaded as faux kernel */
+#else
 #define CONFIG_SYS_TEXT_BASE	0x80110000
+#endif
 
 /* Generic Interrupt Controller */
 #define CONFIG_GICV2

Thog/u-boot

Signed-off-by: Thog <me@thog.eu>

include/configs/tegra210-common.h
@@ -45,7 +45,12 @@
  * ramdisk_addr_r simply shouldn't overlap anything else. Choosing 33M allows
  *   for the FDT/DTB to be up to 1M, which is hopefully plenty.
  */
-#define CONFIG_LOADADDR 0x80080000
+
+#ifdef CONFIG_CPU_BL_IS_CBOOT
+#define CONFIG_LOADADDR	0x80080000	/* chainloaded as faux kernel */
+#else
+#define CONFIG_LOADADDR	0x80110000
+#endif
 #define MEM_LAYOUT_ENV_SETTINGS \
 	"scriptaddr=0x90000000\0" \
 	"pxefile_addr_r=0x90100000\0" \